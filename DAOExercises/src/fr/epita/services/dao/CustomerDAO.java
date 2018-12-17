0fd9e4380fbb2d3cb7ad6021bd4b7584937387eb
Decayeux <qdecayeux@natsystem.fr>
2018-12-17 20:03:17 +0100

    adding DAO create method

--- DAOExercises/src/fr/epita/services/dao/CustomerDAO.java
+++ DAOExercises/src/fr/epita/services/dao/CustomerDAO.java
@@ -1,5 +1,9 @@
 package fr.epita.services.dao;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -11,7 +15,7 @@ public class CustomerDAO {
 
 	private static final String DB_PASSWORD = "";
 	private static final String DB_USERNAME = "sa";
-	private static final String DB_URI = "C:/Formation/db/h2DS";
+	private static final String DB_URI = "jdbc:h2:C:/Formation/db/h2DS";
 										//"~/test/"
 
 	public void create(Customer customer) {
@@ -21,7 +25,7 @@ public class CustomerDAO {
 			PreparedStatement statement = connection.prepareStatement("INSERT INTO CUSTOMER VALUES(?, ?)"); // setting up the SQL query
 			statement.setString(1, customer.getName()); // passing name parameter to the query
 			statement.setString(2, customer.getAddress()); // passing address parameter to the query
-			statement.executeQuery(); // executing query
+			statement.execute(); // executing query
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
