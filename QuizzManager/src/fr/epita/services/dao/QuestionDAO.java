a04fee674499f570eb5cf52dbb0b94b28fa0495a
Decayeux <qdecayeux@natsystem.fr>
2019-01-07 19:57:10 +0100

    questionDAO update

--- QuizzManager/src/fr/epita/services/dao/QuestionDAO.java
+++ QuizzManager/src/fr/epita/services/dao/QuestionDAO.java
@@ -1,22 +1,24 @@
 package fr.epita.services.dao;
 
+import java.util.List;
+
 import fr.epita.datamodel.Question;
 
 public class QuestionDAO {
 
 	public void create(Question question) {
-		
+		//TODO insert into question table a new element
 	}
 	
-	public Question read(String name) {
-		
+	public List<Question> readByDifficulty(int difficulty) {
+		//TODO get all questions of a given difficulty
 	}
 	
 	public void update(Question question) {
-		
+		//TODO update a given question
 	}
 	
 	public void delete(Question question) {
-		
+		//TODO delete a given question
 	}
 }
