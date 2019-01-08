2ce50e37f0d4dd2528e1240197911c41ed19de73
Decayeux <qdecayeux@natsystem.fr>
2019-01-08 20:15:35 +0100

    create method finalized

--- BankAccountManagement/src/fr/epita/services/dao/StockDAO.java
+++ BankAccountManagement/src/fr/epita/services/dao/StockDAO.java
@@ -1,19 +1,38 @@
 package fr.epita.services.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import fr.epita.datamodel.Stock;
 import fr.epita.services.Configuration;
 
 public class StockDAO {
 
+	private String databaseURL;
+	private String databaseUsername;
+	private String databasePassword;
+	
 	public StockDAO() {
 		Configuration config = Configuration.getInstance();  // loading the properties of the 
 															 // "application.properties" file
 		// TODO use the properties loaded in the config
+		this.databaseURL = config.getPropertyValue("db.url");
+		this.databaseUsername = config.getPropertyValue("db.username");
+		this.databasePassword = config.getPropertyValue("db.password");
 	}
 	
 	public void create(Stock stock) {
-		// TODO method to create new Stock in database
-		// 4 steps : connection, sql statement, parameter fill, execution
+		try {
+			Connection connection = DriverManager.getConnection(this.databaseURL, this.databaseUsername, this.databasePassword);
+			PreparedStatement statement = connection.prepareStatement("INSERT INTO STOCK VALUES(?, ?)");
+			statement.setString(1, stock.getTicker());
+			statement.setInt(2, stock.getQuantity());
+			statement.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
