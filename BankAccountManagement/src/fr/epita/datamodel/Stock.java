8f89231aa2237a2639721cde2301029c00a5cb35
Decayeux <qdecayeux@natsystem.fr>
2019-01-08 19:52:14 +0100

    initial import

---
+++ BankAccountManagement/src/fr/epita/datamodel/Stock.java
@@ -0,0 +1,27 @@
+package fr.epita.datamodel;
+
+public class Stock {
+
+	private String ticker;
+	
+	private int quantity;
+
+	public String getTicker() {
+		return ticker;
+	}
+
+	public void setTicker(String ticker) {
+		this.ticker = ticker;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+	
+}
---
+++ BankAccountManagement/src/fr/epita/services/dao/StockDAO.java
@@ -0,0 +1,15 @@
+package fr.epita.services.dao;
+
+import java.io.File;
+
+import fr.epita.services.Configuration;
+
+public class StockDAO {
+
+	public StockDAO() {
+		Configuration config = Configuration.getInstance();
+		// TODO use the properties loaded in the config
+	}
+	
+	
+}
